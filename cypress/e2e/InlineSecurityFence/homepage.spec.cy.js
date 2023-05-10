b69b7e54059a49c51016484898d68c15566961d9
kurkovnick <63467999+kurkovnick@users.noreply.github.com>
2023-05-10 19:19:26 +0300

    Added more Tests

--- cypress/e2e/InlineSecurityFence/homepage.spec.cy.js
+++ cypress/e2e/InlineSecurityFence/homepage.spec.cy.js
@@ -12,6 +12,7 @@ describe('Home Page Tests', () => {
     cy.url().should( 'eq' , 'https://inlinesecurityfence.com/quote/')
   })
 
+
   it('"View Our Fence Styles" Link Takes to the Quotes Page', () => {
     // Visit the website
     cy.visit('https://www.inlinesecurityfence.com')
@@ -23,6 +24,7 @@ describe('Home Page Tests', () => {
     cy.url().should( 'eq' , 'https://inlinesecurityfence.com/fences/')
   })
 
+
   it('Get a Free Quote Link Takes to the Quotes Page', () => {
     // Visit the website
     cy.visit('https://www.inlinesecurityfence.com')
@@ -32,6 +34,40 @@ describe('Home Page Tests', () => {
     cy.get( '[data-id="61f9c09a"] a' )
       .click();
     cy.url().should( 'eq' , 'https://inlinesecurityfence.com/about/')
+  })
+
+  it('"Explore now" for Steel Fence takes to Services Page', () => {
+    // Visit the website
+    cy.visit('https://www.inlinesecurityfence.com')
+
+    // Select the Get a Free Quote
+    
+    cy.get( '[aria-label="Explore Steel Fences"]' )
+      .click();
+    cy.url().should( 'eq' , 'https://inlinesecurityfence.com/fences/#steel')
+  })
+
+
+  it('"Explore now" for Chain Link takes to Services Page', () => {
+    // Visit the website
+    cy.visit('https://www.inlinesecurityfence.com')
+
+    // Select the Get a Free Quote
+    
+    cy.get( '[aria-label="Explore Chain Link Fences"]' )
+      .click();
+    cy.url().should( 'eq' , 'https://inlinesecurityfence.com/fences/#ChainLink')
+  })
+
+  it('"Explore now" for Anti Climb takes to Services Page', () => {
+    // Visit the website
+    cy.visit('https://www.inlinesecurityfence.com')
+
+    // Select the Get a Free Quote
+    
+    cy.get( '[aria-label="Explore Chain Link Fences"]' )
+      .click();
+    cy.url().should( 'eq' , 'https://inlinesecurityfence.com/fences/#AntiClimb')
   })
 })
 

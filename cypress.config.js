e5ae8b681de809437acbbc8dff802b9125895d5b
kurkovnick <63467999+kurkovnick@users.noreply.github.com>
2023-05-14 10:33:35 +0300

    updte configs

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,9 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      require('cypress-email-results')(on, config, {
+        email: ['nick@itishniki.com'],
+      })
     },
     defaultCommandTimeout : 100000,
     pageLoadTimeout : 160000,
